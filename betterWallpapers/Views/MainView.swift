f6b8121203655991e947e859ea7ca870d7f62015
amolon615 <114776102+amolon615@users.noreply.github.com>
2023-02-13 13:32:26 +0100

    changes

--- betterWallpapers/Views/MainView.swift
+++ betterWallpapers/Views/MainView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 import SceneKit
-
+import RevenueCat
 
 
 
---
+++ betterWallpapers/betterWallpapers 2.swift
@@ -0,0 +1,20 @@
+//
+//  BetterWallpapersApp.swift
+//  betterWallpapers
+//
+//  Created by Artem on 14/12/2022.
+//
+
+import SwiftUI
+
+
+@main
+struct BetterWallpapers: App {
+    @StateObject var vm = WallpapersViewModel()
+    var body: some Scene {
+        WindowGroup {
+            ViewFlows()
+                .environmentObject(vm)
+        }
+    }
+}
